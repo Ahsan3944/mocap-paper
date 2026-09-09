a207770d020c541082f0f144aae21a6bd507e34c
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-09 20:17:25 +0530

    Add on-change-dimension recording policy

---
+++ src/main/java/com/ultraop/mocap/recording/OnChangeDimension.java
@@ -0,0 +1,8 @@
+package com.ultraop.mocap.recording;
+
+/** Behaviour applied when the recorded player changes dimension. */
+public enum OnChangeDimension {
+    NOTHING,
+    END_RECORDING,
+    SPLIT_RECORDING
+}

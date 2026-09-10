a9d652d768273316aa84a6715b838301b14e68f2
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-10 18:45:37 +0530

    Fix missing Player import in EntityPlaybackActor

--- src/main/java/com/ultraop/mocap/playback/EntityPlaybackActor.java
+++ src/main/java/com/ultraop/mocap/playback/EntityPlaybackActor.java
@@ -19,6 +19,7 @@ import org.bukkit.craftbukkit.entity.CraftEntity;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
